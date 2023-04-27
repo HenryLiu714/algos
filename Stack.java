6320a35ef83f49601980508f7bdd3f16965375c5
HenryLiu714 <henryliu714@gmail.com>
2023-04-27 19:50:25 -0400

    stack update

--- Stack.java
+++ Stack.java
@@ -1,5 +1,33 @@
+import java.util.Iterator;
+
 public class Stack<T> {
 	private int N; // size of the stack
 	private Node<T> head;
-	private Node<T> next;
+
+	public Stack() {
+		N = 0;
+		head = null;
+	}
+
+	public void push(T val) {
+		head = new Node<T>(val, head);
+		N++;
+	}
+
+	public T pop(){
+		T val = head.val;
+
+		head = head.next;
+		N--;
+
+		return val;
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public boolean isEmpty() {
+		return N == 0;
+	}
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,16 @@
 public class Test {
+
+	static void stackTests() {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(1);
+		stack.push(2);
+
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+	}
+
 	public static void main(String [] args) {
-		Node<Integer> node = new Node<Integer>(5);
-		System.out.println(node.val);
+		stackTests();
 	}
 }
